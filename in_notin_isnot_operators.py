c4fe12afa263b780c9abc3270caa40070affc90e
Abu Jubair <64865506+AbuJubair7@users.noreply.github.com>
2020-08-23 23:51:22 +0600

    Add files via upload
    example of in/not in operator also have 'is not' for non literal object like 'None'

---
+++ in_notin_isnot_operators.py
@@ -0,0 +1,32 @@
+# given example of in/not in / is not operators
+# 'in' operator is basically used for a find a value from a list/string
+# 'is not' operator only used for no literal object like 'None'
+
+grocery = ["Mango", "Apple", "Banana"]
+
+if "Mango" in grocery:
+    print(True)
+else:
+    print(False)
+
+if "Juice" not in grocery:
+    print(True)
+else:
+    print(False)
+
+name = "Juabir"
+
+if "J" in name:
+    print(True)
+else:
+    print(False)
+
+print("c" in name)
+
+flag = True if 3 > 4 else False  # ternary operator
+print(flag)
+
+s = None
+print("True" if s is not None else False)  # similar with print("True" if s != None else False)
+
+
